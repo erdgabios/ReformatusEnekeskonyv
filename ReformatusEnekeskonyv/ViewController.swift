59cc7776dd4241ae125d25b8fdebfaac595d19cb
gaberdios <gaberdios@gmail.com>
2016-10-03 19:04:37 +0200

    Initial Commit

---
+++ ReformatusEnekeskonyv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReformatusEnekeskonyv
+//
+//  Created by G on 03/10/2016.
+//  Copyright © 2016 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
